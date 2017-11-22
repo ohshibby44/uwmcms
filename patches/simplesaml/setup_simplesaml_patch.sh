e39a371a172db31287b65a88814e100d3776757e
sodacrackers <meyer4834@gmail.com>
2017-11-22 13:31:04 -0800

    UWM-CMS: Changing SimpleSAMLphp and removing the root, custom library. Now autoloads with the dependency listed in composer's simplesamlphp_auth entry. Adding our patches in composer.json.

---
+++ patches/simplesaml/setup_simplesaml_patch.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+#
+# Copy default for defaul install.
+#
+
+echo "UWM-CMS: Seeting up vendor SimpleSAMLphp default configurations."
+
+SAMLDIR=vendor/simplesamlphp/simplesamlphp
+if [ ! -d $SIMPSAML ]; then
+    SAMLDIR=../../vendor/simplesamlphp/simplesamlphp
+
+fi
+
+cp -rf $SAMLDIR/config-templates/* $SAMLDIR/config/
+cp -rf $SAMLDIR/metadata-templates/* $SAMLDIR/metadata/
+
+
+
+#
+# Copy custom configuration to four files needed.
+#
+
+echo "UWM-CMS: Copying custom SimpleSAMLphp configuration to default configuration files."
+
+
+DESTINATION=$SAMLDIR/config/config.php
+CODE=`cat <<"EOF"
+
+    /***
+     * Include UWMCMS configurations.
+     *
+     ***/
+    $config_file = sprintf('/mnt/gfs/%s.%s/nobackup/cmsdev.uwmedicine.org/simplesamlphp/config/acquia_config.php',
+        $_ENV['AH_SITE_GROUP'],
+        $_ENV['AH_SITE_ENVIRONMENT']);
+    if (!file_exists($config_file) || !require_once($config_file)) {
+
+        throw new Exception('Missing config file: '. \$config_file);
+
+    }
+
+EOF
+`
+echo "$CODE" >> $DESTINATION
+
+
+#
+# Next:
+#
+DESTINATION=$SAMLDIR/config/authsources.php
+CODE=`cat <<"EOF"
+
+    /***
+     * Include UWMCMS configurations.
+     *
+     ***/
+    $config_file = sprintf('/mnt/gfs/%s.%s/nobackup/cmsdev.uwmedicine.org/simplesamlphp/config/authsources.php',
+        $_ENV['AH_SITE_GROUP'],
+        $_ENV['AH_SITE_ENVIRONMENT']);
+    if (!file_exists($config_file) || !require_once($config_file)) {
+
+        throw new Exception('Missing config file: '. $config_file);
+
+    }
+
+EOF
+`
+echo "$CODE" >> $DESTINATION
+
+
+
+#
+# Next:
+#
+DESTINATION=$SAMLDIR/metadata/saml20-idp-remote.php
+CODE=`cat <<"EOF"
+
+    /***
+     * Include UWMCMS configurations.
+     *
+     ***/
+    $config_file = sprintf('/mnt/gfs/%s.%s/nobackup/cmsdev.uwmedicine.org/simplesamlphp/metadata/saml20-idp-remote.php',
+        $_ENV['AH_SITE_GROUP'],
+        $_ENV['AH_SITE_ENVIRONMENT']);
+    if (!file_exists($config_file) || !require_once($config_file)) {
+
+        throw new Exception('Missing config file: '. $config_file);
+
+    }
+
+EOF
+`
+echo "$CODE" >> $DESTINATION
+
+
+
+
+
+
+
+set +v
