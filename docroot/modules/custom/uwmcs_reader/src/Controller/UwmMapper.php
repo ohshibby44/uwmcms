24fc5c7aa9c281b4320383a3fb170986b78bb416
sodacrackers <meyer4834@gmail.com>
2018-01-25 23:10:28 -0800

    UWM-CMS: Adding location and education tabs for providers. Includes draft uwmcms_reader field mapping module.

---
+++ docroot/modules/custom/uwmcs_reader/src/Controller/UwmMapper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\uwmcs_reader\Controller;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * Controller connects API fields to Drupal fields.
+ */
+class UwmMapper {
+
+  /**
+   * Description here.
+   *
+   * @param string|null $apiFieldValue
+   *   Description here.
+   *
+   * @param string|null $nodePathAlias
+   *   Description here.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|mixed|null|static
+   *   Description here.
+   */
+  public function loadApiNode(string $apiFieldValue = NULL, string $nodePathAlias = NULL) {
+
+    if (!empty($apiFieldValue)) {
+
+      // @TODO: Trim search, in case url it http, https, etc.
+      $nodes = \Drupal::entityTypeManager()
+        ->getStorage('node')
+        ->loadByProperties(['field_information_manager_url' => $apiFieldValue]);
+
+      if ($node = reset($nodes)) {
+
+        return $node;
+
+      }
+
+    }
+
+    if (empty($node) && !empty($nodePathAlias)) {
+
+      $path = \Drupal::service('path.alias_manager')
+        ->getPathByAlias($nodePathAlias);
+      if (preg_match('/node\/(\d+)/', $path, $matches)) {
+
+        return Node::load($matches[1]);
+
+      }
+
+    }
+
+  }
+
+}
