f40ef5e53b6a5201eaeb4c2fb1c254f125b32838
Brian Tofte-Schumacher <hello@briantschu.com>
2018-02-26 20:38:21 -0800

    Bugfix/blitz part deux (#298)
    * UWM-CMS: Fax number improvements
    
    * UWM-CMS: Improve video performance, display video full screen on touch devices
    
    * UWM-CMS: Correct syntax for typeof check
    
    * UWM-CMS: Remove angled edge
    
    * UWM-CMS: Improve nav tabs for tiny screens
    
    * UWM-CMS: Homepage fixes for IE
    
    * UWM-CMS: Improve feature story appearance
    
    * UWM-CMS: Fix image display

--- docroot/themes/custom/uwmed/src/js/patient-care-video.js
+++ docroot/themes/custom/uwmed/src/js/patient-care-video.js
@@ -1,27 +1,87 @@
 (function ($, Drupal) {
   Drupal.behaviors.patientCareVideo = {
     attach: function () {
-      jQuery('#patientCarePlayButton').colorbox(
+
+      // $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
+      //   var state = document.fullScreen || document.mozFullScreen || document.webkitDisplayingFullScreen;
+      //   var event = state ? 'FullscreenOn' : 'FullscreenOff';
+      //
+      //   // Now do something interesting
+      //   console.log(state);
+      // });
+      var video = $('#patientCareVideoPlayer')[0];
+      var touchEvents = ('ontouchstart' in document.documentElement);
+
+      if(typeof(video.play) === "undefined") {
+        // hide the play button because this browser can't play this video
+        $(".homepage-section__story-link").hide();
+      }
+
+      if(screenfull.enabled && touchEvents) {
+        // we can play full screen and this device has touch events, likely a mobile
+        screenfull.on('change', function () {
+          if (!screenfull.isFullscreen) {
+            video.pause();
+            $('#patientCareVideo').parents().first().hide();
+            console.log('full screen over');
+          }
+        });
+
+        $('#patientCarePlayButton').on('click', function () {
+          $('#patientCareVideo').parents().first().show();
+          screenfull.request(video);
+          video.play();
+        });
+      } else if( typeof(video.webkitEnterFullscreen) === "function" && touchEvents ) {
+        // some iOS devices are not covered by screenfull
+        $(video).on('webkitendfullscreen',function() {
+          video.pause();
+          $('#patientCareVideo').parents().first().hide();
+        });
+
+        $('#patientCarePlayButton').on('click', function(){
+          $('#patientCareVideo').parents().first().show();
+          video.webkitEnterFullscreen();
+          video.play();
+        });
+
+        $(video).on('ended', function() {
+          if (!video.webkitDisplayingFullscreen) {
+            $('#patientCareVideo').parents().first().hide();
+          }
+        });
+        $(video).on('pause', function() {
+          if (!video.webkitDisplayingFullscreen) {
+            $('#patientCareVideo').parents().first().hide();
+          }
+        });
+      } else {
+        // we're on a desktop or this device doesn't support full screen,
+        // let's display the video in a colorbox
+
+        jQuery('#patientCarePlayButton').colorbox(
           {
             inline: true,
-            width: '60%',
+            width: '75%',
             height: 'auto',
             href: '#patientCareVideo',
             scrolling: false,
             onOpen: function () {
               $('.page-node-type-homepage .homepage-section__story-link .faa-burst.animated').css('animation-play-state', 'paused')
+              video.focus();
             },
             onClosed: function () {
               $('.page-node-type-homepage .homepage-section__story-link .faa-burst.animated').css('animation-play-state', 'running')
             },
             onComplete: function () {
-              document.getElementById('patientCareVideoPlayer').play();
+              video.play();
             },
             onCleanup: function () {
-              document.getElementById('patientCareVideoPlayer').pause();
+              video.pause();
             }
           }
-      );
+        );
+      }
     }
   }
 })(jQuery, Drupal);
