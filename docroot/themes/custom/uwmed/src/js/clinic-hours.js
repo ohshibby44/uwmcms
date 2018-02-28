dbd151e09e23ae7f4c38cb261505f226d4455236
sodacrackers <meyer4834@gmail.com>
2018-02-28 09:27:39 -0800

    UWM-CMS: Fix making list toggles consistent.

--- docroot/themes/custom/uwmed/src/js/clinic-hours.js
+++ docroot/themes/custom/uwmed/src/js/clinic-hours.js
@@ -13,13 +13,13 @@
 
             var nodeId = $('div#main-container article.uwm-clinic')
                 .attr('data-node-id');
-            var $hrsDiv = $('.clinic-hours-wrapper.hidden');
+            var $hrsDiv = $('.clinic-hours-wrapper');
 
             if (nodeId > 0 && $hrsDiv.length > 0) {
 
                 $.get('/locations/location-hours/' + nodeId, function (data) {
 
-                    var $newHrs = $('.clinic-hours-wrapper.hidden', data);
+                    var $newHrs = $('.clinic-hours-wrapper', data);
                     if ($newHrs.length > 0) {
                         $hrsDiv.html($newHrs.html()).removeClass('hidden');
                     }
