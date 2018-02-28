0129621948ac574e87045071477123f930868cd3
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2018-02-28 12:39:01 -0800

    Features/twig extensions (#305)
    * UWM-CMS: Adding clinic fax and styling. Adding JS for non-cached display of clinic hours.
    
    * UWM-CMS: Adding view page showing clinic hours. Allows for non-cached hours.
    
    * UWM-CMS: Updating SimpleSAMLphp patches to match no-backups repository.
    
    * UWM-CMS: Bug fixes for administrator theme, clinics, locations and nav-tabs.
    
    * UWM-CMS: Adding provider certifications and degrees. Bug fixes for backgrounds and tablet.
    
    * UWM-CMS: Header fix tablet breakpoint and wrap.
    
    * UWM-CMS: Locations background fix; proper striped png needed.
    
    * UWM-CMS: Bug fixes for providers and locations.
    
    * UWM-CMS: Reverting accidental commit.
    
    * UWM-CMS: Adding handling for publications and json-p request. Adding publications to providers.
    
    * UWM-CMS: Adding list padding. Also styling for publications.
    
    * UWM-CMS: Adding scripts for toggling of any class. Adding view more toggle on clinics.
    
    * UWM-CMS: Locations phone and fax fix.
    
    * UWM-CMS: More list styling. Removing title hyperlink on clinics.
    
    * UWM-CMS: Adding button and list styling on services.
    
    * UWM-CMS: Bug fix for view more/less text.
    
    * UWM-CMS: Adding clockwisemd attribute tag. Finds clockwise clinic waiting time when added to html.
    
    * UWM-CMS: Hotfix for twig/ server 500 error.
    
    * UWM-CMS: Adding clinic hours on provider pages.
    
    * UWM-CMS: Fix making list toggles consistent.
    
    * UWM-CMS: Adding structure for custom twig functions.
    
    * UWM-CMS: Namespacing custom twig extensions.

---
+++ docroot/modules/custom/uwmcs_extension/src/TwigExtension.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\uwmcs_extension;
+
+/**
+ * A Twig extension that adds custom functions/filters.
+ */
+class TwigExtension extends \Twig_Extension {
+
+  /**
+   * Generates a list of all Twig functions that this extension defines.
+   *
+   * @return array
+   *   A key/value array that defines custom Twig functions. The key denotes the
+   *   function name used in the tag, e.g.:
+   *
+   * @code
+   *   {{ uwm_test_func() }}
+   * @endcode
+   *
+   *   The value is a standard PHP callback that defines what the function does.
+   */
+  public function getFunctions() {
+    return [
+      new \Twig_SimpleFunction('uwm_test_func', [$this, 'testFunction']),
+    ];
+  }
+
+  /**
+   * Generates a list of all Twig filters that this extension defines.
+   *
+   * @return array
+   *   A key/value array that defines custom Twig filters. The key denotes the
+   *   filter name used in the tag, e.g.:
+   *
+   * @code
+   *   {{ foo|uwm_test_filter }}
+   * @endcode
+   *
+   *   The value is a standard PHP callback that defines what the filter does.
+   */
+  public function getFilters() {
+    return [
+      new \Twig_SimpleFilter('uwm_test_filter', [$this, 'testFilter']),
+
+    ];
+  }
+
+  /**
+   * Gets a unique identifier for this Twig extension.
+   *
+   * @return string
+   *   A unique identifier for this Twig extension.
+   */
+  public function getName() {
+    return 'uwmcs_extension';
+  }
+
+  /**
+   * Description text.
+   *
+   * @param bool $upperCase
+   *   Description text.
+   *
+   * @return string
+   *   Description text.
+   */
+  public static function testFunction($upperCase = FALSE) {
+    $string = "The quick brown box jumps over the lazy dog 123.";
+    if ($upperCase) {
+      return strtoupper($string);
+    }
+    else {
+      return strtolower($string);
+    }
+  }
+
+  /**
+   * Description text.
+   *
+   * @param string $string
+   *   Description text.
+   *
+   * @return string
+   *   Description text.
+   */
+  public static function testFilter(string $string) {
+    return strtolower($string);
+  }
+
+}
