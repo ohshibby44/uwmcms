a3deb2d5f8d7be66fdb3dc6fcd667100e000155e
sodacrackers <meyer4834@gmail.com>
2018-03-05 10:37:21 -0800

    UWM-CMS: Configuration export from prod. Enabling custom twig extension and redirect modules.

--- docroot/modules/custom/uwmcs_extension/src/TwigExtension.php
+++ docroot/modules/custom/uwmcs_extension/src/TwigExtension.php
@@ -137,13 +137,13 @@ class TwigExtension extends \Twig_Extension {
   public static function convertInlineStyles(string $string) {
 
     $patterns = [
-      '/style="[^"]?italic[^>]+>([^<]+)/',
-      '/style="[^"]?bold[^>]+>([^<]+)/',
+      '/(style="[^"]?italic[^>]+>)([^<]+)/',
+      '/(style="[^"]?bold[^>]+>)([^<]+)/',
     ];
 
     $replacements = [
-      '<em>$1</em>',
-      '<strong>$1</strong>',
+      '$1<em>$2</em>',
+      '$1<strong>$2</strong>',
     ];
 
     return preg_replace($patterns, $replacements, $string);
