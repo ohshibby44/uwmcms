777ea3956127e0a7028b10439e3e3799292b9e0e
Brian Tofte-Schumacher <hello@briantschu.com>
2018-03-07 18:46:21 -0800

    UWM-CMS: More specific rules for hiding the play button on iPhone 4 (#326)

--- docroot/themes/custom/uwmed/src/js/patient-care-video.js
+++ docroot/themes/custom/uwmed/src/js/patient-care-video.js
@@ -5,9 +5,12 @@
       var video = $('#patientCareVideoPlayer')[0];
       var touchEvents = ('ontouchstart' in document.documentElement);
 
-      if(typeof(video.play) === "undefined" || !video.duration > 0) {
-        // hide the play button because this browser can't play this video
-        $(".homepage-section__story-link").hide();
+      if(typeof(video.play) === "undefined" || (!video.duration > 0)) {
+        if(typeof(video.duration !== "undefined")) {
+          // hide the play button because this browser can't play this video
+          $(".homepage-section__story-link").hide();
+          $("#patientCarePlayButton").hide();
+        }
       }
 
       if(screenfull.enabled && touchEvents) {
