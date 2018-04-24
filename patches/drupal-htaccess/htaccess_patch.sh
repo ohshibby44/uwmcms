7dc41e03748c52483fc7b8b330f687b133376ed9
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2018-04-24 09:53:53 -0700

    UWM-CMS: Trying alternative patching script. (#363)

--- patches/drupal-htaccess/htaccess_patch.sh
+++ patches/drupal-htaccess/htaccess_patch.sh
@@ -14,21 +14,23 @@ patch_file=$1
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo "UWM-CMS: Trying patches:"
-echo "patch -p1 -N --dry-run --silent < $patch_file 2>/dev/null"
+echo "UWM-CMS: Running htaccess_patch.sh to add our custom htaccess rules."
+test=`patch -p1 -N --dry-run --silent < $patch_file 2>&1`
 
-
-patch -p1 -N --dry-run --silent < $patch_file 2>/dev/null
 
 # If the patch has not been applied then the $? which is the exit status
 # for last command would have a success status code = 0
 
+
 if [ $? -eq 0 ];
 then
 
-    echo "UWM-CMS: Okay patch return; applying patch:"
-    echo "patch -p1 -N < $patch_file"
-
+    echo "UWM-CMS: Applying our patch now:"
+    echo "UWM-CMS: patch -p1 -N < $patch_file"
     patch -p1 -N < $patch_file
+
+else
+    echo "UWM-CMS: $test"
+    echo "UWM-CMS: Seems the patch was already applied."
 
 fi
