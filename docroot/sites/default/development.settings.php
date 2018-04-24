cfe999c8f4382524773f1d13fdac22ecec081715
sodacrackers <meyer4834@gmail.com>
2018-04-23 19:51:56 -0700

    UWM-CMS: Removing broken database config from settings.php.

--- docroot/sites/default/development.settings.php
+++ docroot/sites/default/development.settings.php
@@ -1,11 +1,5 @@
 <?php
-$databases['default']['default'] = array(
-  'driver' => 'mysql',
-  'database' => 'default',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'host' => 'localhost',
-);
+
 
 
 $config['system.performance']['css']['preprocess'] = FALSE;
@@ -14,6 +8,5 @@ $config['system.performance']['minifyhtml']['minify_html'] = FALSE;
 
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-
 
 $settings['cache']['bins']['page'] = 'cache.backend.null';
