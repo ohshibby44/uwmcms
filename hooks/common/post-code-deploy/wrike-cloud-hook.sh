6ab1423d5c00e5744bfacf12471102b98fa94624
sodacrackers <meyer4834@gmail.com>
2018-05-15 11:51:16 -0700

    UWM-CMS: Updating wrike post-deploy hook to use github log, rather than empty Aquia git log.

--- hooks/common/post-code-deploy/wrike-cloud-hook.sh
+++ hooks/common/post-code-deploy/wrike-cloud-hook.sh
@@ -26,6 +26,7 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 
+github_token=5b07de54cee80e913411387a61e7c1a0c049846f
 wrike_client_id="saDHmzPz"
 wrike_account_api_token="2URYEzRHqgFtEnYeJvk9faKInRLktJs3yFuHobjGkWqoMgUJc8F46tqTUUpcFNqc-N-WFIUKC"
 wrike_account_task_folder_id="IEABOA5QI4HA3PRF"
@@ -34,15 +35,57 @@ wrike_account_task_folder_id="IEABOA5QI4HA3PRF"
 # Successful deploy.
 if [ "$source_branch" != "$deployed_tag" ]; then
 
+
+      github_branch=$(sed "s|-build||g" <<< $deployed_tag)
+
       summary_text="UWM-CMS: A deployment has been made to *$site.$target_env* using tag *$deployed_tag* and source *$source_branch*"
+      
+      body_text="<p>More can be found at: <br>"
+      body_text+="<a href=\"https://github.com/uwmweb/uwmcms\">Github repo</a> or<br>"
+      body_text+="<a href=\"https://github.com/uwmweb/uwmcms/commits/$github_branch\">https://github.com/uwmweb/uwmcms/commits/$github_branch</a></p>"
 
-      git_log=`git log --pretty="<b>%h...%d:</b> %s %b" --max-count=40 --tags=$deployed_tag`
 
-      body_text=`echo -e "<em>$summary_text</em> \n\n\n<code>$git_log</code>\n\nAlso see $repo_url. . . ." | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/<br>/g'`
+      github_query=$(cat <<EOF
+      repository(owner: \"uwmweb\", name: \"uwmcms\") {
+        object(expression: \"$github_branch\") {
+          ... on Commit {
+            history(first: 30) {
+              edges {
+                node {
+                  abbreviatedOid
+                  committedDate
+                  message
+                }
+              }
+            }
+          }
+        }
+      }
+EOF
+)
+
+
+      github_response=$(curl -H "Authorization: bearer $github_token" -d @- https://api.github.com/graphql <<EOF
+      {
+          "query": "query { $github_query }"
+      }
+EOF
+)
+
+
+      github_log=$(echo $github_response | tr -d '\n' | tr -d '\r' | ruby -e " \
+      require 'rubygems'; require 'json'; require 'date'; s =''; d = JSON[STDIN.read]; \
+      d['data']['repository']['object']['history']['edges'].each do |cmt| \
+        puts '<p><b>' + cmt['node']['abbreviatedOid'] + '...</b>' + \
+          Date.parse(cmt['node']['committedDate']).strftime('%a, %d %b %Y') + '...' + \
+          cmt['node']['message'] + '</p>'; \
+        end; puts s;")
+
+
 
       curl -g -X POST -H "Authorization: bearer $wrike_account_api_token" \
             --data "title=$summary_text" \
-            --data-binary "description=$body_text" \
+            --data-binary "description=$body_text<br><br><br><code>$github_log</code>" \
             "https://www.wrike.com/api/v3/folders/$wrike_account_task_folder_id/tasks"
 
 
