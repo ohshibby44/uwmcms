a52609d7c19a2c40ca9f63c35432f2688bfe1462
sodacrackers <meyer4834@gmail.com>
2018-07-06 16:13:49 -0700

    UWM-CMS: Adding gulp-include allow adding files from ./node_modules folder. Adding JS placeholder for clinic open hours.

--- docroot/themes/custom/uwmed/src/js/clinic-hours.js
+++ docroot/themes/custom/uwmed/src/js/clinic-hours.js
@@ -12,9 +12,18 @@
 
         attach: function (context, settings) {
 
+            var $elm = $('[uwm-opens-next]', context);
 
+            $elm.each(function (a, b) {
+
+                var opens = $(b).attr('data-opens');
+                var dateTimeString = moment(opens).format("DD-MM-YYYY HH:mm:ss");
+                var timeString = moment(opens).format("HH:mm:ss");
+
+            });
 
         }
+
     };
 
 })(jQuery, Drupal, drupalSettings);
