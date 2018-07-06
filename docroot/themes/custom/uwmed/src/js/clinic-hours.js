c9d472bdf6e17a36f6247afb99dfa2a46697f388
sodacrackers <meyer4834@gmail.com>
2018-07-06 09:16:43 -0700

    UWM-CMS: Adding partial work fr clinic open hours.

--- docroot/themes/custom/uwmed/src/js/clinic-hours.js
+++ docroot/themes/custom/uwmed/src/js/clinic-hours.js
@@ -9,23 +9,11 @@
 
 
     Drupal.behaviors.showCurrentHours = {
+
         attach: function (context, settings) {
 
-            var nodeId = $('div#main-container article.uwm-clinic')
-                .attr('data-node-id');
-            var $hrsDiv = $('.clinic-hours-wrapper');
 
-            if (nodeId > 0 && $hrsDiv.length > 0) {
 
-                $.get('/locations/location-hours/' + nodeId, function (data) {
-
-                    var $newHrs = $('.clinic-hours-wrapper', data);
-                    if ($newHrs.length > 0) {
-                        $hrsDiv.html($newHrs.html()).removeClass('hidden');
-                    }
-
-                });
-            }
         }
     };
 
