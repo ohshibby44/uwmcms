4014407de94940dd7b0425e10914def1f97d6f31
sodacrackers <meyer4834@gmail.com>
2018-07-09 12:51:56 -0700

    UWM-CMS: Disabling twig cache on dev. Adding option to include JavaScript files, with babel or preprocessing. Adding formatting for clinic hours matching prod.

--- docroot/sites/default/development.settings.php
+++ docroot/sites/default/development.settings.php
@@ -1,6 +1,7 @@
 <?php
 
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/development.services.yml';
+
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['cache']['bins']['page'] = 'cache.backend.null';
--- docroot/themes/custom/uwmed/src/js/clinic-hours.js
+++ docroot/themes/custom/uwmed/src/js/clinic-hours.js
@@ -3,103 +3,130 @@
  */
 
 
-(function($, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings) {
 
-  'use strict';
-
-
-  Drupal.behaviors.showCurrentHours = {
-
-    attach: function(context, settings) {
-
-      var $elm = $('[data-uwm-opens-at]', context);
-
-      $elm.each(function(a, b) {
-
-        var $this = $(b);
-        var data = $this.data('uwm-opens-at');
-        var markup = hoursMarkup(data, $this.is('[data-show-brief]'));
-
-        $this.html(markup);
-
-      });
-
-    }
-
-  };
+    'use strict';
 
 
-  function uwf() {
+    Drupal.behaviors.showCurrentHours = {
 
-    var s = arguments[0],
-      i = arguments.length;
+        attach: function (context, settings) {
 
-    while (i--) {
-      s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
-    }
-    return s;
+            var $elm = $('[data-uwm-opens-at]', context);
 
-  }
+            $elm.each(function (a, b) {
+
+                var $this = $(b);
+                var data = $this.data('uwm-opens-at');
+                var markup = hoursMarkup(data, $this.is('[data-show-brief]'));
+
+                $this.find('span').html(markup);
+
+            });
+
+        }
+
+    };
 
 
-  function hoursMarkup(data, showBrief) {
+    function uwf() {
 
-    var now = moment().format('X') * 1;
-    var opens = now > data.closes ? data.nextOpens : data.opens;
-    var closes = now > data.closes ? data.nextCloses : data.closes;
-    var markup = '';
+        var s = arguments[0],
+            i = arguments.length;
 
-    // Opens later:
-    if (now < opens) {
-
-      if (showBrief) {
-        markup = uwf(
-          '<em><strong>Closed - </strong> Opens {1} at {2}</em>',
-          moment.unix(opens).format('ddd'),
-          moment.unix(opens).format('h:mm')
-        );
-      } else {
-        markup = uwf(
-          '<em><strong>Closed - </strong> Opens {1} at {2}</em>',
-          moment.unix(opens).format('dddd'),
-          moment.unix(opens).format('h:mm a')
-        );
-      }
-
-    }
-    // Opens soon:
-    if (now < opens && now + 3600 > opens) {
-
-      markup = uwf(
-        '<em><strong>Opens soon - </strong> {1}</em>',
-        moment.unix(opens).toNow()
-      );
-
-    }
-    // Open now
-    if (now > opens && now < closes) {
-
-      markup = uwf(
-        '<em><strong>Open - </strong> Closes at {1}</em>',
-        moment.unix(closes).format('h:mm a')
-      );
-
-    }
-    // Closes soon: 
-    else if (now > opens && now < closes && now + 3600 > closes) {
-
-      markup = uwf(
-        '<em><strong>Closes soon</strong> {0}</em>',
-        moment.unix(closes).toNow()
-      );
+        while (i--) {
+            s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+        }
+        return s;
 
     }
 
-    return markup;
 
-  }
+    function hoursMarkup(data, showBrief) {
 
+        var now = moment().format('X') * 1;
+        var opens = now > data.closes ? data.nextOpens : data.opens;
+        var closes = now > data.closes ? data.nextCloses : data.closes;
+        var dayToday = moment().format('dddd');
+        var dayOpens = moment.unix(opens).format('dddd');
 
+        var markup = '';
+
+        // Opens later:
+        if (now < opens) {
+
+            if (showBrief) {
+                markup = uwf('<strong>Closed -</strong> Opens {1} at {2}',
+                    moment.unix(opens).format('ddd'),
+                    moment.unix(opens).format('h:mm')
+                );
+            } else {
+                markup = uwf('<strong>Closed -</strong> Opens {1} at {2}',
+                    moment.unix(opens).format('dddd'),
+                    moment.unix(opens).format('h:mm a')
+                );
+            }
+
+        }
+
+        // Opens today:
+        if (now < opens && dayOpens === dayToday && showBrief) {
+
+            markup = uwf('<strong>Closed -</strong> Opens today at {1}',
+                moment.unix(opens).format('h:mm')
+            );
+
+        }
+        // Opens soon:
+        if (now < opens && now + 3600 > opens) {
+
+            if (showBrief) {
+                markup = uwf('<strong>Opens soon -</strong> {1}',
+                    moment.unix(opens).toNow()
+                );
+            } else {
+                markup = uwf('<strong>Opens soon:</strong> <em>{1} - {2}</em>',
+                    moment.unix(opens).format('h:mm a'),
+                    moment.unix(closes).format('h:mm a')
+                );
+            }
+
+        }
+        // Open now:
+        if (now > opens && now < closes) {
+
+            if (showBrief) {
+                markup = uwf('<strong>Open -</strong> Closes at {1}',
+                    moment.unix(closes).format('h:mm a')
+                );
+            }
+            else {
+                markup = uwf('<strong>Open now:</strong> <em>Closes at {1}</em>',
+                    moment.unix(closes).format('h:mm a')
+                );
+
+            }
+
+        }
+        // Closes soon:
+        else if (now > opens && now < closes && now + 3600 > closes) {
+
+            if (showBrief) {
+                markup = uwf('<strong>Closes soon -</strong> {0}',
+                    moment.unix(closes).toNow()
+                );
+            }
+            else {
+                markup = uwf('<strong>Closes soon: </strong> <em>{0}</em>',
+                    moment.unix(closes).toNow()
+                );
+            }
+
+        }
+
+        return markup;
+
+    }
 
 
 })(jQuery, Drupal, drupalSettings);
