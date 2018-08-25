899b447b66599c9a394977fe96ee73d3f77f2044
sodacrackers <meyer4834@gmail.com>
2018-08-25 00:35:35 -0700

    UWM-CMS: Refining general search layout and refiners.

--- docroot/themes/custom/uwmed/src/custom-packages/search/search.js
+++ docroot/themes/custom/uwmed/src/custom-packages/search/search.js
@@ -3,16 +3,25 @@
  * Custom JavaScript for UW Medicine.
  */
 
-(function ($, Drupal) {
+(function($, Drupal) {
 
 
-    Drupal.behaviors.uwmSearch = {
+  Drupal.behaviors.uwmSearchLayout = {
 
-        attach: function (context, settings) {
+    attach: function(context, settings) {
 
-        }
+      var $searchField = $('.form-item.form-item-search-api-fulltext.form-group', context);
+      var $searchFacet = $('section#block-uwmsearchwithinstring div.facets-widget', context).clone(true);
 
-    };
+      $searchField.once('rearrange').each(function() {
+            $searchField.after($searchFacet);
+      });
+        
+
+    }
+
+  };
+
 
 
 })(jQuery, Drupal);
