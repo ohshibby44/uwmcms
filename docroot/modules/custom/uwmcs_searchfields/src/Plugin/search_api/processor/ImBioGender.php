03a9b303c94e3ebe84212ace25ac1160f26475d5
sodacrackers <meyer4834@gmail.com>
2018-09-28 14:31:43 -0700

    UWM-CMS: Adding search modules and configs as a fresh install.

---
+++ docroot/modules/custom/uwmcs_searchfields/src/Plugin/search_api/processor/ImBioGender.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\uwmcs_searchfields\Plugin\search_api\Processor;
+
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+use Drupal\uwmcs_searchfields\Controller\UwmSearchInfoMgrHelper;
+
+/**
+ * Adds the item's URL to the indexed data.
+ *
+ * @SearchApiProcessor(
+ *   id = "ImBioGender",
+ *   label = @Translation("ImBioGender"),
+ *   description = @Translation("Provides additional UWM values to search."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = false,
+ * )
+ */
+class ImBioGender extends ProcessorPluginBase {
+
+  const FACET_NAME = 'ImBioGender';
+
+  const FIELD_NAME = 'gender';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+
+    $properties = [];
+
+    if (!$datasource) {
+
+      $definition = [
+        'label' => self::FACET_NAME,
+        'description' => $this->t('Provider extra values for search.'),
+        'type' => 'string',
+        'processor_id' => $this->getPluginId(),
+      ];
+
+      $properties[strtolower(self::FACET_NAME)] = new ProcessorProperty($definition);
+
+    }
+
+    return $properties;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+
+    $entity = $item->getOriginalObject(TRUE)->getValue();
+    if ($entity->getType() === 'uwm_provider') {
+
+      $data = UwmSearchInfoMgrHelper::getEntityApiData($entity);
+      $newValues = UwmSearchInfoMgrHelper::extractAllApiMatches($data, self::FIELD_NAME);
+
+      $fields = $item->getFields(FALSE);
+      $fields = $this->getFieldsHelper()
+        ->filterForPropertyPath($fields, NULL,
+          strtolower(self::FACET_NAME));
+
+      foreach ($newValues as $value) {
+        foreach ($fields as $field) {
+          if (!empty($value)) {
+            $field->addValue($value);
+          }
+        }
+      }
+
+    }
+
+  }
+
+}
